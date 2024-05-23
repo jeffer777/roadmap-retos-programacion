4f16fefd65ab04bf55adf64848ee17adf4568123
jeffer777 <106620997+jeffer777@users.noreply.github.com>
2024-05-22 20:03:53 -0500

    Create jeffer777.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/jeffer777.py
@@ -0,0 +1,28 @@
+# https://www.python.org/downloads/windows/
+# = Comentario
+"""
+Comentario en 
+varias lineas
+"""
+'''
+Mismo comentario en lineas
+solo cambia las comillas
+'''
+# Variable
+Jeff=3
+# Constante: python no tiene constantes, a menos que sea una varialbe denominado con carateres
+f = 'lolazo'
+
+#Datos primitivos
+#--Enteros
+Int = 3
+#--Flotante
+Fl = 3.4
+#--Booleanos
+T = True
+F = False
+#--Caracter
+C = 'Caracteres'
+
+# Imprimir
+print('Hola, Python')
